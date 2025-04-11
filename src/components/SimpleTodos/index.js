e03220ab73e02e6e2ed2ffa645954cd15bc962c3
daaspraveen <dasaripraveenkumar2000@gamail.com>
2025-04-11 22:28:20 +0000

    developed enhanced version

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -39,27 +39,98 @@ const initialTodosList = [
 
 // Write your code here
 class SimpleTodos extends Component {
-  state = {todoList: initialTodosList}
+  state = {todoList: initialTodosList, todoInput: ''}
+
+  addTodo = () => {
+    const {todoInput, todoList} = this.state
+    // console.log('todoList', todoList)
+    const getnewId = todoList.length > 0 ? todoList[todoList.length - 1].id : 0
+    // console.log(getnewId)
+    const numberMatch = todoInput.match(/(\d+)$/)
+    const numberOfTodos = numberMatch ? parseInt(numberMatch[0], 10) : 1 // Default to 1 if no number is found
+    const onlyString = todoInput.replace(/(\d+)$/, '').trim()
+
+    const newTodo = []
+    let i = 0
+    while (newTodo.length < numberOfTodos) {
+      newTodo.push({
+        id: getnewId + 1 + i,
+        title: onlyString,
+      })
+      i += 1
+    }
+    // console.log(newTodo)
+    this.setState(prev => ({
+      todoList: [...prev.todoList, ...newTodo],
+      todoInput: '',
+    }))
+  }
 
   deleteTodo = id => {
     const {todoList} = this.state
     this.setState({todoList: todoList.filter(eachItem => eachItem.id !== id)})
   }
 
+  saveTodo = (id, newTodoValue) => {
+    this.setState(prev => ({
+      todoList: prev.todoList.map(each => {
+        if (each.id === id) {
+          return {...each, title: newTodoValue}
+        }
+        return each
+      }),
+    }))
+  }
+
+  checkedTodo = id => {
+    // console.log('in checked func')
+    this.setState(prev => ({
+      todoList: prev.todoList.map(each => {
+        if (each.id === id) {
+          return {...each, checked: !each.checked || false}
+        }
+        return each
+      }),
+    }))
+  }
+
+  handleKeyDown = e => {
+    const {todoInput} = this.state
+    const {key} = e
+    // console.log(key)
+    if (key === 'Enter' && todoInput) {
+      this.addTodo()
+    }
+  }
+
   render() {
-    const {todoList} = this.state
+    const {todoInput, todoList} = this.state
     return (
       <div className="container">
         <div className="card">
           <h1 className="heading">Simple Todos</h1>
+          <div className="input-box">
+            <input
+              type="text"
+              className="todo-input"
+              value={todoInput}
+              onChange={e => this.setState({todoInput: e.target.value})}
+              onKeyDown={this.handleKeyDown}
+              placeholder="Enter Your Todo"
+            />
+            <button type="button" className="add-btn" onClick={this.addTodo}>
+              Add
+            </button>
+          </div>
           <ul className="todos-box">
             {todoList.map(eachItem => (
               <TodoItem
                 key={eachItem.id}
-                id={eachItem.id}
-                title={eachItem.title}
+                todoDetails={eachItem}
                 list={todoList}
                 deleteTodo={this.deleteTodo}
+                saveTodo={this.saveTodo}
+                checkedTodo={this.checkedTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,19 +1,61 @@
 // Write your code here
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {id, title, deleteTodo} = props
+  const {todoDetails, deleteTodo, saveTodo, checkedTodo} = props
+  const {id, title, checked = false} = todoDetails
+  const [isEdit, setIsEdit] = useState(false)
+  const [inputEdit, setInputEdit] = useState(title)
+
+  const doChecked = () => {
+    checkedTodo(id)
+  }
 
   const doDelete = () => {
     deleteTodo(id)
   }
 
+  const doRunEditSave = () => {
+    if (isEdit) {
+      saveTodo(id, inputEdit)
+    }
+    setIsEdit(prev => !prev)
+  }
+
   return (
     <li className="todo-list">
-      <p className="todo-para">{title}</p>
-      <button className="todo-delete-btn" type="button" onClick={doDelete}>
-        Delete
-      </button>
+      {!isEdit && (
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={doChecked}
+          className="checkbox"
+        />
+      )}
+      {isEdit ? (
+        <input
+          type="text"
+          className="input-edit"
+          value={inputEdit}
+          onChange={e => setInputEdit(e.target.value)}
+        />
+      ) : (
+        <p
+          className="todo-para"
+          style={{textDecoration: checked ? 'line-through' : 'none'}}
+        >
+          {title}
+        </p>
+      )}
+      <div className="btns-box">
+        <button className="saveEdit-btn" type="button" onClick={doRunEditSave}>
+          {isEdit ? 'Save' : 'Edit'}
+        </button>
+        <button className="todo-delete-btn" type="button" onClick={doDelete}>
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
